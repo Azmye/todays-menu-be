065edb503465a525c5db88d536ca09afc0964967
azmye <mhmmdazmye@gmail.com>
2025-01-19 21:36:15 +0700

    refactor: change framework to hono

--- src/db/schema/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as role, roleRelations } from "./role";
-export { default as user } from "./user";
-export { default as userRole, userRoleRelations } from "./userRole";
-export { default as store, storeRelations } from "./store";
